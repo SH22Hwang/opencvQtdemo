a8d5c29dc4d922145e128d97db965443050b70c3
SH22Hwang <70620484+SH22Hwang@users.noreply.github.com>
2021-07-02 14:14:53 +0900

    미완성..

--- liveLabel3.py
+++ liveLabel3.py
@@ -46,7 +46,7 @@ class App(QWidget):
         vbox = QVBoxLayout()
         vbox.addWidget(self.image_label)
         vbox.addWidget(self.textLabel)
-        
+
         # set the vbox layout as the widgets layout
         self.setLayout(vbox)
 
@@ -61,14 +61,12 @@ class App(QWidget):
         self.thread.stop()
         event.accept()
 
-
-
     @pyqtSlot(np.ndarray)
     def update_image(self, cv_img):
         """Updates the image_label with a new opencv image"""
         qt_img = self.convert_cv_qt(cv_img)
         self.image_label.setPixmap(qt_img)
-    
+
     def convert_cv_qt(self, cv_img):
         """Convert from an opencv image to QPixmap"""
         rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
@@ -77,8 +75,9 @@ class App(QWidget):
         convert_to_Qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
         p = convert_to_Qt_format.scaled(self.disply_width, self.display_height, Qt.KeepAspectRatio)
         return QPixmap.fromImage(p)
-    
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     a = App()
     a.show()
--- staticLabel2.py
+++ staticLabel2.py
@@ -1,9 +1,9 @@
 from PyQt5 import QtWidgets, QtGui, QtCore
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QVBoxLayout, QHBoxLayout
-from PyQt5.QtGui import QPixmap, QImage, QColor
-from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt,QObject
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QVBoxLayout, QHBoxLayout, QRadioButton, QPushButton
+from PyQt5.QtGui import QPixmap, QImage, QColor, QIcon
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, Qt, QObject
 import sys
-import cv2
+import cv2 as cv
 
 
 class App(QWidget):
@@ -14,7 +14,7 @@ class App(QWidget):
         self.display_height = 480
         # create the label that holds the image
         self.image_label = QLabel(self)
-        #self.image_label.resize(self.disply_width, self.display_height)
+        # self.image_label.resize(self.disply_width, self.display_height)
         # create a text label
         self.textLabel = QLabel('Polecat')
 
@@ -25,29 +25,28 @@ class App(QWidget):
         # set the vbox layout as the widgets layout
         self.setLayout(vbox)
         # don't need the grey image now
-        #grey = QPixmap(self.disply_width, self.display_height)
-        #grey.fill(QColor('darkGray'))
-        #self.image_label.setPixmap(grey)
+        # grey = QPixmap(self.disply_width, self.display_height)
+        # grey.fill(QColor('darkGray'))
+        # self.image_label.setPixmap(grey)
 
         # load the test image - we really should have checked that this worked!
-        cv_img = cv2.imread('polecat.jpg')
+        cv_img = cv.imread('polecat.jpg')
         # convert the image to Qt format
         qt_img = self.convert_cv_qt(cv_img)
         # display it
         self.image_label.setPixmap(qt_img)
 
-
-    
     def convert_cv_qt(self, cv_img):
         """Convert from an opencv image to QPixmap"""
-        rgb_image = cv2.cvtColor(cv_img, cv2.COLOR_BGR2RGB)
+        rgb_image = cv.cvtColor(cv_img, cv.COLOR_BGR2RGB)
         h, w, ch = rgb_image.shape
         bytes_per_line = ch * w
         convert_to_Qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
         p = convert_to_Qt_format.scaled(self.disply_width, self.display_height, Qt.KeepAspectRatio)
         return QPixmap.fromImage(p)
-    
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     a = App()
     a.show()
